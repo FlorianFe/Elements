6eede7a9cf468c40fcd7e3e903968e54a32727bf
Tobi Obeck <13554426+TobiObeck@users.noreply.github.com>
2019-10-27 11:33:48 +0100

    fixes layout bug when leaving maximized window state #7
    Changing a maximized window to normal state fires 'move' and 'unmaximize' (not used) events. In that moment a redraw and reflow of the web document is forced so that the responsive layout is recalculated and applied properly. This is done via hiding and - after a delay - showing the root element. The move event is also continuously fired when dragging the window around. Since this event fires so often, constantly redrawing the page with 'show/hide' results in distracting flickering. Therefore it is saved in a variable if the window was maximized and not yet rerendered. The rerender is debounced to prevent the flickering. While in the window-still-dragging-state the window is already rendered blurred with the wrong fullscreen layout to give the user a feedback that a rerender has not yet happened and he has to release the drag to see the content

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const electron = require('electron');
+const debounce = require('lodash.debounce')
 
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
@@ -9,107 +10,132 @@ const Menu = electron.Menu;
 
 const path = require('path');
 
-let browserWindow = null;
+let window = null;
 
-function createWindow()
-{
-  browserWindow = new BrowserWindow(
-  {
+function createWindow() {
+  window = new BrowserWindow({
     width: 800,
     height: 545,
     backgroundColor: '#fff'
   });
+  window.loadURL(path.join('file://', __dirname, 'index.html'));
 
-  browserWindow.loadURL(path.join('file://', __dirname, 'index.html'));
+  function forceRedraw() {
+    window.webContents.executeJavaScript(
+      `document.getElementById('periodic-table').style.display = 'none';`);
 
-  browserWindow.on('closed', () =>
-  {
-    browserWindow = null;
+    setTimeout(() => {
+      window.webContents.executeJavaScript(
+        `document.getElementById('periodic-table').style.display = '';
+        document.getElementsByTagName('body')[0].style.filter = '';`);
+    }, 20);
+  }
+
+  let isRedrawApplied = true;
+  const debouncedRedraw = debounce(() => {
+    isRedrawApplied = true;
+    forceRedraw();
+  }, 200);
+
+  window.on('maximize', () => {
+    isRedrawApplied = false;
+  });
+
+  window.on('move', () => {
+    if (!window.isMaximized() && !isRedrawApplied) {
+      window.webContents.executeJavaScript(
+        `document.getElementsByTagName('body')[0].style.filter = 'blur(5px)';`);
+      debouncedRedraw();
+    }
+  });
+
+  window.on('closed', () => {
+    window = null;
   });
 
   const template =
-  [
-    {
-      label: 'Edit',
-      submenu: [
-        {role: 'undo'},
-        {role: 'redo'},
-        {type: 'separator'},
-        {role: 'cut'},
-        {role: 'copy'},
-        {role: 'paste'},
-        {role: 'pasteandmatchstyle'},
-        {role: 'delete'},
-        {role: 'selectall'}
-      ]
-    },
-    {
-      label: 'View',
-      submenu: [
-        {role: 'reload'},
-        {role: 'forcereload'},
-        {role: 'toggledevtools'},
-        {type: 'separator'},
-        {role: 'resetzoom'},
-        {role: 'zoomin'},
-        {role: 'zoomout'},
-        {type: 'separator'},
-        {role: 'togglefullscreen'}
-      ]
-    },
-    {
-      role: 'window',
-      submenu: [
-        {role: 'minimize'},
-        {role: 'close'}
-      ]
-    },
-    {
-      role: 'help',
-      submenu: [
-        {
-          label: 'Learn More',
-          click () { require('electron').shell.openExternal('https://github.com/FlorianFe/Elements') }
-        }
-      ]
-    }
-  ]
+    [
+      {
+        label: 'Edit',
+        submenu: [
+          { role: 'undo' },
+          { role: 'redo' },
+          { type: 'separator' },
+          { role: 'cut' },
+          { role: 'copy' },
+          { role: 'paste' },
+          { role: 'pasteandmatchstyle' },
+          { role: 'delete' },
+          { role: 'selectall' }
+        ]
+      },
+      {
+        label: 'View',
+        submenu: [
+          { role: 'reload' },
+          { role: 'forcereload' },
+          { role: 'toggledevtools' },
+          { type: 'separator' },
+          { role: 'resetzoom' },
+          { role: 'zoomin' },
+          { role: 'zoomout' },
+          { type: 'separator' },
+          { role: 'togglefullscreen' }
+        ]
+      },
+      {
+        role: 'window',
+        submenu: [
+          { role: 'minimize' },
+          { role: 'close' }
+        ]
+      },
+      {
+        role: 'help',
+        submenu: [
+          {
+            label: 'Learn More',
+            click() { require('electron').shell.openExternal('https://github.com/FlorianFe/Elements') }
+          }
+        ]
+      }
+    ]
 
   if (process.platform === 'darwin') {
     template.unshift({
       label: app.getName(),
       submenu: [
-        {role: 'about'},
-        {type: 'separator'},
-        {role: 'services', submenu: []},
-        {type: 'separator'},
-        {role: 'hide'},
-        {role: 'hideothers'},
-        {role: 'unhide'},
-        {type: 'separator'},
-        {role: 'quit'}
+        { role: 'about' },
+        { type: 'separator' },
+        { role: 'services', submenu: [] },
+        { type: 'separator' },
+        { role: 'hide' },
+        { role: 'hideothers' },
+        { role: 'unhide' },
+        { type: 'separator' },
+        { role: 'quit' }
       ]
     })
 
     // Edit menu
     template[1].submenu.push(
-      {type: 'separator'},
+      { type: 'separator' },
       {
         label: 'Speech',
         submenu: [
-          {role: 'startspeaking'},
-          {role: 'stopspeaking'}
+          { role: 'startspeaking' },
+          { role: 'stopspeaking' }
         ]
       }
     )
 
     // Window menu
     template[3].submenu = [
-      {role: 'close'},
-      {role: 'minimize'},
-      {role: 'zoom'},
-      {type: 'separator'},
-      {role: 'front'}
+      { role: 'close' },
+      { role: 'minimize' },
+      { role: 'zoom' },
+      { type: 'separator' },
+      { role: 'front' }
     ]
   }
 
@@ -118,23 +144,18 @@ function createWindow()
 }
 
 
-app.on('ready', () =>
-{
+app.on('ready', () => {
   createWindow();
 });
 
-app.on('activate', function ()
-{
-  if (browserWindow === null)
-  {
+app.on('activate', function () {
+  if (window === null) {
     createWindow()
   }
 });
 
-app.on('window-all-closed', () =>
-{
-  if(process.platform !== 'darwin')
-  {
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
     app.quit()
   }
 });
